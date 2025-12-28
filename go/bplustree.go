68b1a4549b7255ba0f49b6ee09f212ccc4fee55f
chisato <chisato-x@bupt.edu.cn>
2025-12-28 22:31:31 +0800

    1. bplustree go

---
+++ go/bplustree.go
@@ -0,0 +1,184 @@
+package main
+
+type BPlusTree[T comparable] struct {
+	size   int
+	height int
+	degree int
+	root   *bNode[T]
+	cmpF   func(T, T) int
+
+	opt int
+}
+
+type bNode[T comparable] struct {
+	elem       []T
+	child      []*bNode[T]
+	prev, next *bNode[T]
+}
+
+type bNodeSplit[T comparable] struct {
+	elem     T
+	lnd, rnd *bNode[T]
+}
+
+func NewBPlusTree[T comparable](degree int, cmpF func(T, T) int) *BPlusTree[T] {
+	return &BPlusTree[T]{
+		size:   0,
+		height: 0,
+		degree: degree,
+		root:   nil,
+		cmpF:   cmpF,
+	}
+}
+
+func (t *BPlusTree[T]) Insert(e T) {
+	t.opt = 1
+	if split := t.insert(t.root, e); split != nil {
+		newRoot := newBNode([]T{split.elem}, nil)
+		if split.lnd != nil && split.rnd != nil {
+			newRoot.child = append(newRoot.child, split.lnd, split.rnd)
+		}
+		t.root = newRoot
+		t.height++
+	}
+	t.size += t.opt
+}
+
+func (t *BPlusTree[T]) insert(nd *bNode[T], e T) *bNodeSplit[T] {
+	if nd == nil {
+		return &bNodeSplit[T]{elem: e}
+	}
+
+	geIndex, eq := t.getGEIndex(nd, e)
+	if len(nd.child) == 0 { // leafNode
+		if eq {
+			nd.elem[geIndex] = e
+			t.opt = 0
+			return nil
+		} else {
+			nd.insertElem(geIndex, e)
+		}
+	} else { // internalNode
+		if split := t.insert(nd.child[geIndex], e); split != nil {
+			nd.insertElem(geIndex, split.elem)
+			nd.insertChild(geIndex, split.lnd, split.rnd)
+		}
+	}
+
+	if len(nd.elem) == t.degree {
+		return nd.split()
+	}
+
+	return nil
+}
+
+func (t *BPlusTree[T]) getGEIndex(nd *bNode[T], e T) (ge int, eq bool) {
+	geIndex := len(nd.elem)
+	for i := range nd.elem {
+		if cmpRes := t.cmpF(nd.elem[i], e); cmpRes == 0 {
+			return i, true
+		} else if cmpRes > 0 {
+			return i, false
+		}
+	}
+	return geIndex, false
+}
+
+func newBNode[T comparable](elem []T, child []*bNode[T]) *bNode[T] {
+	emcpy := make([]T, len(elem))
+	chcpy := make([]*bNode[T], len(child))
+	copy(emcpy, elem)
+	copy(chcpy, child)
+	return &bNode[T]{
+		elem:  emcpy,
+		child: chcpy,
+	}
+}
+
+func (nd *bNode[T]) split() *bNodeSplit[T] {
+	d := len(nd.elem)
+	var lnd, rnd *bNode[T]
+	if len(nd.child) > 0 { // internalNode
+		lnd = newBNode(nd.elem[:d/2], nd.child[:d/2+1])
+		rnd = newBNode(nd.elem[d/2+1:], nd.child[d/2+1:])
+	} else { // leafNode
+		lnd = newBNode(nd.elem[:d/2], nil)
+		rnd = newBNode(nd.elem[d/2:], nil)
+	}
+	lnd.next = rnd
+	rnd.prev = lnd
+
+	return &bNodeSplit[T]{
+		elem: nd.elem[d/2],
+		lnd:  lnd,
+		rnd:  rnd,
+	}
+}
+
+func (nd *bNode[T]) insertElem(i int, e T) {
+	nd.elem = append(nd.elem, e)
+	for idx := len(nd.elem) - 1; idx > i; idx-- {
+		nd.elem[idx] = nd.elem[idx-1]
+	}
+	nd.elem[i] = e
+}
+
+func (nd *bNode[T]) insertChild(i int, lnd, rnd *bNode[T]) {
+	nd.child = append(nd.child, lnd)
+	for idx := len(nd.child) - 1; idx > i; idx-- {
+		nd.child[idx] = nd.child[idx-1]
+	}
+	nd.child[i] = lnd
+	nd.child[i+1] = rnd
+}
+
+func (t *BPlusTree[T]) Delete(e T) {
+
+}
+
+func (t *BPlusTree[T]) Get(e T) (T, bool) {
+	for nd := t.root; nd != nil; {
+		if geIndex, eq := t.getGEIndex(nd, e); eq {
+			if len(nd.child) == 0 {
+				return nd.elem[geIndex], true
+			} else {
+				nd = nd.child[geIndex+1]
+			}
+		} else {
+			nd = nd.child[geIndex]
+		}
+	}
+	return e, false
+}
+
+func (t *BPlusTree[T]) minNode() *bNode[T] {
+	for nd := t.root; nd != nil; {
+		if len(nd.child) == 0 {
+			return nd
+		}
+		nd = nd.child[0]
+	}
+	return nil
+}
+
+func (t *BPlusTree[T]) Iterate(vis func(T) bool) {
+	for cur := t.minNode(); cur != nil; cur = cur.next {
+		for _, e := range cur.elem {
+			if !vis(e) {
+				return
+			}
+		}
+	}
+}
+
+// internalNode: keyNum: [ halfUpper(d)-1, d-1 ], childNum: keyNum+1
+// leafNode: keyNum: [ halfUpper(d), d-1 ]
+// so Split: keyNum == d
+// so Merge: keyNum == halfUpper(d)-1
+func halfUpper(x int) int {
+	halfx := x >> 1
+	if x%2 == 1 {
+		halfx += 1
+	}
+	return halfx
+}
