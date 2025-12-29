aa018f42baf579aecc7ea1c6cd73648748ef0b8e
chisato <chisato-x@bupt.edu.cn>
2025-12-29 10:04:13 +0000

    1. bplustree insert

--- go/bplustree.go
+++ go/bplustree.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type BPlusTree[T comparable] struct {
 	size   int
 	height int
@@ -31,6 +33,14 @@ func NewBPlusTree[T comparable](degree int, cmpF func(T, T) int) *BPlusTree[T] {
 	}
 }
 
+func (t *BPlusTree[T]) Size() int {
+	return t.size
+}
+
+func (t *BPlusTree[T]) Height() int {
+	return t.height
+}
+
 func (t *BPlusTree[T]) Insert(e T) {
 	t.opt = 1
 	if split := t.insert(t.root, e); split != nil {
@@ -59,6 +69,9 @@ func (t *BPlusTree[T]) insert(nd *bNode[T], e T) *bNodeSplit[T] {
 			nd.insertElem(geIndex, e)
 		}
 	} else { // internalNode
+		if eq {
+			geIndex += 1
+		}
 		if split := t.insert(nd.child[geIndex], e); split != nil {
 			nd.insertElem(geIndex, split.elem)
 			nd.insertChild(geIndex, split.lnd, split.rnd)
@@ -105,8 +118,9 @@ func (nd *bNode[T]) split() *bNodeSplit[T] {
 		lnd = newBNode(nd.elem[:d/2], nil)
 		rnd = newBNode(nd.elem[d/2:], nil)
 	}
-	lnd.next = rnd
-	rnd.prev = lnd
+	linkBNode(nd.prev, lnd)
+	linkBNode(lnd, rnd)
+	linkBNode(rnd, nd.next)
 
 	return &bNodeSplit[T]{
 		elem: nd.elem[d/2],
@@ -130,10 +144,48 @@ func (nd *bNode[T]) insertChild(i int, lnd, rnd *bNode[T]) {
 	}
 	nd.child[i] = lnd
 	nd.child[i+1] = rnd
+	//if i > 0 {
+	//	linkBNode(nd.child[i-1], nd.child[i])
+	//}
+	//if i+2 < len(nd.child) {
+	//	linkBNode(nd.child[i+1], nd.child[i+2])
+	//}
 }
 
 func (t *BPlusTree[T]) Delete(e T) {
+	t.opt = 0
+	t.delete(nil, t.root, e)
+	t.size += t.opt
+}
 
+func (t *BPlusTree[T]) delete(parent, nd *bNode[T], e T) {
+	if nd == nil {
+		return
+	}
+
+	geIndex, eq := t.getGEIndex(nd, e)
+	if len(nd.child) == 0 { // leafNode
+		if eq {
+			t.opt = -1
+			nd.deleteElem(geIndex)
+		} else {
+			return
+		}
+	} else { // internalNode
+		if eq {
+			geIndex += 1
+		}
+		t.delete(nd, nd.child[geIndex], e)
+	}
+
+	if len(nd.elem) == halfUpper(t.degree)-1 {
+
+	}
+
+}
+
+func (nd *bNode[T]) deleteElem(i int) {
+	nd.elem = append(nd.elem[:i], nd.elem[i+1:]...)
 }
 
 func (t *BPlusTree[T]) Get(e T) (T, bool) {
@@ -168,6 +220,37 @@ func (t *BPlusTree[T]) Iterate(vis func(T) bool) {
 				return
 			}
 		}
+	}
+}
+
+func (t *BPlusTree[T]) levelNode(level int) *bNode[T] {
+	for l, cur := 1, t.root; cur != nil && l <= min(t.height, level); l++ {
+		if l == level || len(cur.child) == 0 {
+			return cur
+		}
+		cur = cur.child[0]
+	}
+	return nil
+}
+
+func (t *BPlusTree[T]) IterateLevel(level int, vis func(T) bool) {
+	for cur := t.levelNode(level); cur != nil; cur = cur.next {
+		fmt.Printf("[ ")
+		for _, e := range cur.elem {
+			if !vis(e) {
+				return
+			}
+		}
+		fmt.Printf(" ]")
+	}
+}
+
+func linkBNode[T comparable](lnd, rnd *bNode[T]) {
+	if lnd != nil {
+		lnd.next = rnd
+	}
+	if rnd != nil {
+		rnd.prev = lnd
 	}
 }
 
