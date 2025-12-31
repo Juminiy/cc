2c11c367aace73a12bd4ffcebb5b9d4a13c72e3a
chisato <chisato-x@bupt.edu.cn>
2025-12-31 08:13:45 +0000

    1. bplustree delete fix downup, indexreplace, but fail

--- go/bplustree/bplustree.go
+++ go/bplustree/bplustree.go
@@ -58,7 +58,15 @@ func (t *BPlusTree[T]) Insert(e T) {
 
 func (t *BPlusTree[T]) Delete(e T) {
 	t.opt = 0
-	t.delete(nil, -1, t.root, e)
+	t.delete(t.root, e)
+	if len(t.root.elem) == 0 {
+		if len(t.root.child) > 0 {
+			t.root = t.root.child[0]
+		} else {
+			t.root = nil
+		}
+		t.height -= 1
+	}
 	t.size += t.opt
 }
 
@@ -87,13 +95,22 @@ func (t *BPlusTree[T]) Iterate(vis func(T) bool) {
 	}
 }
 
-func (t *BPlusTree[T]) IterateLevel(level int, vis func(T) bool) {
+func (t *BPlusTree[T]) IterateLevel(level int, vis func(T) bool, nodeBrace ...func()) {
+	b0fn, b1fn := func() {}, func() {}
+	if len(nodeBrace) > 0 {
+		b0fn, b1fn = nodeBrace[0], nodeBrace[0]
+	}
+	if len(nodeBrace) > 1 {
+		b1fn = nodeBrace[1]
+	}
 	for cur := t.levelNode(level); cur != nil; cur = cur.next {
+		b0fn()
 		for _, e := range cur.elem {
 			if !vis(e) {
 				return
 			}
 		}
+		b1fn()
 	}
 }
 
@@ -128,7 +145,7 @@ func (t *BPlusTree[T]) insert(nd *bNode[T], e T) *bNodeSplit[T] {
 	return nil
 }
 
-func (t *BPlusTree[T]) delete(parent *bNode[T], curIndex int, nd *bNode[T], e T) { // *bNode[T]
+func (t *BPlusTree[T]) delete(nd *bNode[T], e T) { // *bNode[T]
 	if nd == nil {
 		return
 	}
@@ -138,47 +155,50 @@ func (t *BPlusTree[T]) delete(parent *bNode[T], curIndex int, nd *bNode[T], e T)
 		if eq {
 			t.opt = -1
 			nd.deleteElem(geIndex)
-		} else {
-			return
 		}
 	} else { // internalNode
-		if eq && geIndex+1 < len(nd.child) { // eq: index replacement
-			geIndex += 1
+		chIndex, chNode := geIndex, nd.child[geIndex]
+		if eq { // eq: index replacement, both in internalNode and leafNode
+			chIndex, chNode = geIndex+1, nd.child[geIndex+1]
+			nd.updateIndex(geIndex, chNode)
 		}
-		t.delete(nd, geIndex, nd.child[geIndex], e)
+		t.delete(chNode, e)
+		if len(chNode.elem) < t.keyMinCnt {
+			t.borrowOrMerge(nd, chIndex, chNode)
+		}
 	}
+}
 
-	if parent != nil && len(nd.elem) < t.keyMinCnt { // node lacks key
-		hasLeftSib, hasRightSib := curIndex-1 >= 0, curIndex+1 < len(parent.child)
+// parent.child[curIndex] = nd
+func (t *BPlusTree[T]) borrowOrMerge(parent *bNode[T], curIndex int, nd *bNode[T]) {
+	hasLeftSib, hasRightSib := curIndex-1 >= 0, curIndex+1 < len(parent.child)
 
-		if hasLeftSib {
-			if nd.borrowLeft(parent, curIndex, t.keyMinCnt) { // borrow ok
-				return
-			}
-		}
-		if hasRightSib {
-			if nd.borrowRight(parent, curIndex, t.keyMinCnt) { // borrow ok
-				return
-			}
-		}
-		if hasLeftSib {
-			leftSibling := parent.child[curIndex-1]
-			leftSibling.merge(parent.elem[curIndex-1], nd)
-			linkBNode(leftSibling, nd.next)
-			parent.deleteChild(curIndex)
-			parent.deleteElem(curIndex - 1)
-			return
-		}
-		if hasRightSib {
-			rightSibling := parent.child[curIndex+1]
-			nd.merge(parent.elem[curIndex], rightSibling)
-			linkBNode(nd, rightSibling.next)
-			parent.deleteChild(curIndex + 1)
-			parent.deleteElem(curIndex)
+	if hasLeftSib {
+		if nd.borrowLeft(parent, curIndex, t.keyMinCnt) { // borrow ok
 			return
 		}
 	}
-
+	if hasRightSib {
+		if nd.borrowRight(parent, curIndex, t.keyMinCnt) { // borrow ok
+			return
+		}
+	}
+	if hasLeftSib {
+		leftSibling := parent.child[curIndex-1]
+		leftSibling.merge(parent.elem[curIndex-1], nd)
+		linkBNode(leftSibling, nd.next)
+		parent.deleteChild(curIndex)
+		parent.deleteElem(curIndex - 1)
+		return
+	}
+	if hasRightSib {
+		rightSibling := parent.child[curIndex+1]
+		nd.merge(parent.elem[curIndex], rightSibling)
+		linkBNode(nd, rightSibling.next)
+		parent.deleteChild(curIndex + 1)
+		parent.deleteElem(curIndex)
+		return
+	}
 }
 
 func (t *BPlusTree[T]) getGEIndex(nd *bNode[T], e T) (ge int, eq bool) {
@@ -194,13 +214,7 @@ func (t *BPlusTree[T]) getGEIndex(nd *bNode[T], e T) (ge int, eq bool) {
 }
 
 func (t *BPlusTree[T]) minNode() *bNode[T] {
-	for nd := t.root; nd != nil; {
-		if len(nd.child) == 0 {
-			return nd
-		}
-		nd = nd.child[0]
-	}
-	return nil
+	return t.root.minLeafNode()
 }
 
 func (t *BPlusTree[T]) levelNode(level int) *bNode[T] {
@@ -322,6 +336,40 @@ func (nd *bNode[T]) deleteElem(i int) T {
 
 func (nd *bNode[T]) deleteChild(i int) {
 	nd.child = append(nd.child[:i], nd.child[i+1:]...)
+}
+
+func (nd *bNode[T]) minElem() (e T, ok bool) {
+	if cur := nd.minLeafNode(); cur != nil && len(cur.elem) > 0 {
+		return cur.elem[0], true
+	}
+	return e, false
+}
+
+func (nd *bNode[T]) minLeafNode() *bNode[T] {
+	for cur := nd; cur != nil; {
+		if len(cur.child) == 0 {
+			return cur
+		}
+		cur = cur.child[0]
+	}
+	return nil
+}
+
+// |nd|
+//
+//	  \
+//	rchNode
+func (nd *bNode[T]) updateIndex(geIndex int, rChNode *bNode[T]) {
+	if leafNode := rChNode.minLeafNode(); leafNode != nil {
+		if leafNode.elem[0] == nd.elem[geIndex] {
+			if len(leafNode.elem) >= 2 {
+				nd.elem[geIndex] = leafNode.elem[1]
+			} else { // must be merged
+			}
+		} else {
+			nd.elem[geIndex] = leafNode.elem[0]
+		}
+	}
 }
 
 func linkBNode[T comparable](lnd, rnd *bNode[T]) {
